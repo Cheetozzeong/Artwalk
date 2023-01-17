8b7891c02f1899055706bcb4cd2640be9625c509
ych526 <ych526@naver.com>
2023-01-17 17:47:39 +0900

    TodayTask: 작성 20230117[S08P12A401-#150]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/MainActivity.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/MainActivity.kt
@@ -3,9 +3,9 @@ package com.a401.artwalk.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SampleActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
+        setContentView(R.layout.activity_main)
     }
 }
