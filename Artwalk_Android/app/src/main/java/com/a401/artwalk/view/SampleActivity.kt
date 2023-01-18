db8b67b5d70267601bda9b41d16797be63636129
ych526 <ych526@naver.com>
2023-01-18 10:04:30 +0900

    Remove: 관련없는 Activity들 삭제[S08P12A401-150]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/SampleActivity.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/SampleActivity.kt
@@ -1,11 +1,16 @@
 package com.a401.artwalk.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.a401.artwalk.R
+import com.a401.artwalk.base.BaseActivity
+import com.a401.artwalk.databinding.SampleViewBinding
+import com.mapbox.maps.Style
 
-class SampleActivity : AppCompatActivity() {
+class SampleActivity: BaseActivity<SampleViewBinding>(R.layout.sample_view) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
+
+        binding.mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
     }
 }
