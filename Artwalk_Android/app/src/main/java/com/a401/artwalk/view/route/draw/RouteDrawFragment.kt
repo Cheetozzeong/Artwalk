ed836e65af6b53649f02768a13d74dec3cef3163
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-01-18 17:34:35 +0900

    Feat: 수정 - route_draw.xml, RouteDrawViewModel 연결 [S08P12A401-187]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/draw/RouteDrawFragment.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/draw/RouteDrawFragment.kt
@@ -2,6 +2,7 @@ package com.a401.artwalk.view.route.draw
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseFragment
 import com.a401.artwalk.databinding.RouteDrawFragmentBinding
@@ -9,12 +10,16 @@ import com.mapbox.maps.Style
 
 class RouteDrawFragment : BaseFragment<RouteDrawFragmentBinding> (R.layout.route_draw_fragment) {
 
+    private val routeDrawViewModel by viewModels<RouteDrawViewModel> { defaultViewModelProviderFactory }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setInitBinding()
         binding.mapViewRouteDraw.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
-        binding.textViewRouteDrawDrawButton.setOnClickListener{
-            it.isSelected = !it.isSelected
-        }
+    }
+
+    private fun setInitBinding() {
+        binding.vm = routeDrawViewModel
     }
 }
