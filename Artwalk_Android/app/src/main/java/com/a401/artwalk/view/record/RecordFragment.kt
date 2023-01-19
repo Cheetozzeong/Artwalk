e2a895cceeca8494cef056d4d15dfb212be2a9bf
ych526 <ych526@naver.com>
2023-01-19 13:26:30 +0900

    Feat: 수정RecordFragment, record_fragment.xml 수정 및 RecordViewModel과 연결 [S08P12A401-151]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/record/RecordFragment.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/record/RecordFragment.kt
@@ -2,29 +2,42 @@ package com.a401.artwalk.view.record
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isGone
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseFragment
 import com.a401.artwalk.databinding.RecordFragmentBinding
 import com.mapbox.maps.Style
+import androidx.fragment.app.viewModels
 
 class RecordFragment : BaseFragment<RecordFragmentBinding>(R.layout.record_fragment) {
+
+    private val recordViewModel by viewModels<RecordViewModel>{defaultViewModelProviderFactory}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setInitBinding()
+        changeStartButtonState()
         binding.mapViewRecord.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
-        binding.imagebuttonRecordStartbutton.setOnClickListener{
-            it.isSelected = !it.isSelected
-            binding.imagebuttonRecordQuitbutton.isSelected= !binding.imagebuttonRecordQuitbutton.isSelected
-            if (binding.imagebuttonRecordQuitbutton.isSelected == true)
-            {
-                binding.imagebuttonRecordQuitbutton.visibility = View.INVISIBLE
-                binding.imagebuttonRecordQuitbutton.isEnabled = true
-            }
-            else
-            {
-                binding.imagebuttonRecordQuitbutton.visibility = View.VISIBLE
-                binding.imagebuttonRecordQuitbutton.isEnabled = false
+    }
+
+    private fun setInitBinding(){
+        binding.vm = recordViewModel
+    }
+
+    private fun changeStartButtonState(){
+        var quitbutton = binding.imagebuttonRecordQuitbutton
+        recordViewModel.startButtonEvent.observe(requireActivity()){
+            with(binding.imagebuttonRecordStartbutton){
+                isSelected= !isSelected
+                quitbutton.isSelected = !quitbutton.isSelected
+                if (quitbutton.isSelected)
+                {
+                    quitbutton.setVisibility(View.GONE);
+                }
+                else{
+                    quitbutton.setVisibility(View.VISIBLE);
+                }
             }
         }
     }
