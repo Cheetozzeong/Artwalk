8f1e6a7454f9916f579afdd463a2e95bc977436a
ych526 <ych526@naver.com>
2023-01-19 17:43:10 +0900

    Feat: [S08P12A401-34]

---
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/login/LoginFragment.kt
@@ -0,0 +1,14 @@
+package com.a401.artwalk.view.login
+
+import android.os.Bundle
+import android.view.View
+import com.a401.artwalk.R
+import com.a401.artwalk.base.BaseFragment
+import com.a401.artwalk.databinding.LoginFragmentBinding
+
+
+class LoginFragment : BaseFragment<LoginFragmentBinding>(R.layout.login_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
