989c864053f495a09603a6b329ec2a31b5eb2c09
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-01-19 16:51:02 +0900

    Feat: 추가 - mapBox 클릭 시 마커 생성 [S08P12A401-16]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/draw/RouteDrawFragment.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/draw/RouteDrawFragment.kt
@@ -1,23 +1,87 @@
 package com.a401.artwalk.view.route.draw
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.viewModels
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseFragment
 import com.a401.artwalk.databinding.FragmentRouteDrawBinding
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 
 class RouteDrawFragment : BaseFragment<FragmentRouteDrawBinding> (R.layout.fragment_route_draw) {
 
     private val routeDrawViewModel by viewModels<RouteDrawViewModel> { defaultViewModelProviderFactory }
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapView: MapView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setInitBinding()
         changeDrawButtonState()
-        binding.mapViewRouteDraw.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+        setMapBoxView()
+    }
+
+    private fun setMapBoxView() {
+        mapView = binding.mapViewRouteDraw
+        mapboxMap = mapView.getMapboxMap()
+
+        mapboxMap.addOnMapClickListener { point ->
+            addAnnotationToMap(point)
+            true
+        }
+        mapboxMap.loadStyleUri(Style.MAPBOX_STREETS)
+    }
+
+    private fun addAnnotationToMap(point: Point) {
+        bitmapFromDrawableRes(
+            requireContext(),
+            R.drawable.ic_route_draw_marker_16
+        )?.let {
+            val annotationApi = mapView.annotations
+            val pointAnnotationManager = annotationApi.createPointAnnotationManager()
+
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+                .withPoint(point)
+                .withIconImage(it)
+
+            pointAnnotationManager.create(pointAnnotationOptions)
+        }
+    }
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
     }
 
     private fun setInitBinding() {
