1c983d363fe15e386503cfd7187f22b0fdf928b9
ych526 <ych526@naver.com>
2023-01-19 13:22:43 +0900

    Feat: 추가RecordViewModel.kt 변수 추가[S08P12A401-151]

---
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/record/RecordViewModel.kt
@@ -0,0 +1,30 @@
+package com.a401.artwalk.view.record
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RecordViewModel : ViewModel() {
+
+    private val _durationHour: MutableLiveData<Int> = MutableLiveData(1)
+    val durationHour: LiveData<Int> = _durationHour
+
+    private val _durationMinute: MutableLiveData<Int> = MutableLiveData(2)
+    val durationMinute: LiveData<Int> = _durationMinute
+
+    private val _durationSecond: MutableLiveData<Int> = MutableLiveData(3)
+    val durationSecond: LiveData<Int> = _durationSecond
+
+    private val _distance: MutableLiveData<Float> = MutableLiveData(0f)
+    val distance: LiveData<Float> = _distance
+
+    private val _startButtonEvent: MutableLiveData<Unit> = MutableLiveData()
+    val startButtonEvent: LiveData<Unit> = _startButtonEvent
+
+    fun onClickStartButton(){
+        _startButtonEvent.value = Unit
+
+    }
+
+}
