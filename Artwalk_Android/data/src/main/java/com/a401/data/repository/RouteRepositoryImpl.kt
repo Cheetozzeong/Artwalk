c1f1189d595fd642da13387025a04c6117ace0a8
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-01-21 23:52:00 +0900

    Feat: 작성중 - mapbox directions 호출 ~ing [S08P12A401-189]

--- Artwalk_Android/data/src/main/java/com/a401/data/repository/RouteRepositoryImpl.kt
+++ Artwalk_Android/data/src/main/java/com/a401/data/repository/RouteRepositoryImpl.kt
@@ -1,17 +1,86 @@
 package com.a401.data.repository
 
+import android.util.Log
+import com.a401.data.BuildConfig
 import com.a401.domain.model.Marker
 import com.a401.domain.model.Route
 import com.a401.domain.repository.RouteRepository
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.MapboxDirections
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.core.constants.Constants
+import com.mapbox.geojson.Point
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
-class RouteRepositoryImpl @Inject constructor(): RouteRepository {
 
-    override fun getRouteBetweenTwoMarker(from: Marker, to: Marker, profile: String): Route {
-        return Route(
-            400.0,
-            300.0,
-            "{\"coordinates\":[[-74.149234,40.935829],[-74.148426,40.935679],[-74.146917,40.940564],[-74.144981,40.940219],[-74.143088,40.940834],[-74.1424,40.940103],[-74.140894,40.939059],[-74.135422,40.937304],[-74.134798,40.936942],[-74.132992,40.933754],[-74.131264,40.929603],[-74.129913,40.927036],[-74.129683,40.927053],[-74.129563,40.927244],[-74.12926,40.927462]]}"
-        )
+class RouteRepositoryImpl @Inject constructor(
+
+): RouteRepository {
+
+    override fun getRoute(
+        from: Marker,
+        to: Marker,
+    ): Route {
+        var route = Route()
+        getRouteBetweenTwoMarker(from, to, successsCallback = {
+            route = it
+        })
+        return route
+    }
+
+    private fun getRouteBetweenTwoMarker(
+        from: Marker,
+        to: Marker,
+        successsCallback: (Route) -> Unit
+    ) {
+
+        val client = MapboxDirections.builder()
+            .routeOptions(
+                RouteOptions.builder()
+                    .coordinatesList(listOf(
+                        Point.fromLngLat(from.longitude, from.latitude),
+                        Point.fromLngLat(to.longitude, to.latitude)
+                    ))
+                    .profile(DirectionsCriteria.PROFILE_WALKING)
+                    .overview(DirectionsCriteria.OVERVIEW_SIMPLIFIED)
+                    .geometries(DirectionsCriteria.GEOMETRY_POLYLINE)
+                    .build()
+            )
+            .accessToken(BuildConfig.MAPBOX_ACCESS_TOKEN)
+            .build()
+
+        client.enqueueCall(object: Callback<DirectionsResponse> {
+
+            override fun onFailure(call: Call<DirectionsResponse?>?, throwable: Throwable) {
+            }
+
+            override fun onResponse(
+                call: Call<DirectionsResponse>,
+                response: Response<DirectionsResponse>,
+            ) {
+                if(response.body() == null) {
+                    TODO("Not yet implemented")
+                    return
+                } else if(response.body()!!.routes().size < 1) {
+                    return
+                }
+
+                response.body()!!.routes().get(0).let {
+                    if (it != null) {
+                        successsCallback(
+                            Route(
+                                it.duration(),
+                                it.distance(),
+                                it.geometry()?:""
+                            )
+                        )
+                    }
+                }
+            }
+        })
     }
 }
