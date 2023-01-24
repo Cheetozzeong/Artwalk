b8bd79f70f70b2f9b80234fad315a1ea5e79024d
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-01-25 00:09:50 +0900

    Feat: 수정 - retrofit으로 싹 변경!  [S08P12A401-189]

--- Artwalk_Android/data/src/main/java/com/a401/data/repository/RouteRepositoryImpl.kt
+++ Artwalk_Android/data/src/main/java/com/a401/data/repository/RouteRepositoryImpl.kt
@@ -1,86 +1,38 @@
 package com.a401.data.repository
 
-import android.util.Log
-import com.a401.data.BuildConfig
+import com.a401.data.datasource.remote.RouteRemoteDataSource
+import com.a401.data.mapper.markerToRequest
+import com.a401.data.model.request.MarkerRequest
 import com.a401.domain.model.Marker
 import com.a401.domain.model.Route
 import com.a401.domain.repository.RouteRepository
 import com.mapbox.api.directions.v5.DirectionsCriteria
-import com.mapbox.api.directions.v5.MapboxDirections
-import com.mapbox.api.directions.v5.models.DirectionsResponse
-import com.mapbox.api.directions.v5.models.RouteOptions
-import com.mapbox.core.constants.Constants
-import com.mapbox.geojson.Point
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import java.lang.Exception
 import javax.inject.Inject
 
 
 class RouteRepositoryImpl @Inject constructor(
-
+    private val routeRemoteDataSource: RouteRemoteDataSource
 ): RouteRepository {
 
-    override fun getRoute(
+    override suspend fun getRouteForWalking(
         from: Marker,
         to: Marker,
-    ): Route {
-        var route = Route()
-        getRouteBetweenTwoMarker(from, to, successsCallback = {
-            route = it
-        })
-        return route
-    }
+    ): Result<Route> = try {
 
-    private fun getRouteBetweenTwoMarker(
-        from: Marker,
-        to: Marker,
-        successsCallback: (Route) -> Unit
-    ) {
+        val coordinates: ArrayList<MarkerRequest> = ArrayList()
+        coordinates.add(markerToRequest(from))
+        coordinates.add(markerToRequest(to))
 
-        val client = MapboxDirections.builder()
-            .routeOptions(
-                RouteOptions.builder()
-                    .coordinatesList(listOf(
-                        Point.fromLngLat(from.longitude, from.latitude),
-                        Point.fromLngLat(to.longitude, to.latitude)
-                    ))
-                    .profile(DirectionsCriteria.PROFILE_WALKING)
-                    .overview(DirectionsCriteria.OVERVIEW_SIMPLIFIED)
-                    .geometries(DirectionsCriteria.GEOMETRY_POLYLINE)
-                    .build()
+        Result.success(
+            routeRemoteDataSource.getRouteData(
+                DirectionsCriteria.PROFILE_WALKING,
+                coordinates,
+                DirectionsCriteria.GEOMETRY_POLYLINE,
+                DirectionsCriteria.OVERVIEW_SIMPLIFIED
             )
-            .accessToken(BuildConfig.MAPBOX_ACCESS_TOKEN)
-            .build()
-
-        client.enqueueCall(object: Callback<DirectionsResponse> {
-
-            override fun onFailure(call: Call<DirectionsResponse?>?, throwable: Throwable) {
-            }
-
-            override fun onResponse(
-                call: Call<DirectionsResponse>,
-                response: Response<DirectionsResponse>,
-            ) {
-                if(response.body() == null) {
-                    TODO("Not yet implemented")
-                    return
-                } else if(response.body()!!.routes().size < 1) {
-                    return
-                }
-
-                response.body()!!.routes().get(0).let {
-                    if (it != null) {
-                        successsCallback(
-                            Route(
-                                it.duration(),
-                                it.distance(),
-                                it.geometry()?:""
-                            )
-                        )
-                    }
-                }
-            }
-        })
+        )
+    } catch (e: Exception) {
+        Result.failure(e)
     }
 }
