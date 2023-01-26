f3b8f23c0105d9376e703b4fb59ccaaa7c002a89
yujj25 <yujj25@naver.com>
2023-01-26 09:58:09 +0900

    Layout: 생성 - 관리자 페이지 로그인 화면 [S08P12A401-210]

--- artwalk_backend/vue-front/src/App.vue
+++ artwalk_backend/vue-front/src/App.vue
@@ -1,33 +1,36 @@
 <template>
   <b-container fluid id="app" class="d-flex">
     <div>
-      <b-button v-b-toggle.sidebar-no-header style="font-size: 1.5rem" variant="light" class="mt-3 justify-content-start">
-        <b-icon icon="list"></b-icon>
+      <b-button v-b-toggle.sidebar-no-header style="font-size: 2rem" variant="light" class="mt-3 justify-content-start">
+        <b-icon icon="list" class="pb-2"></b-icon>
       </b-button>
     </div>
     <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow>
       <template #default="{ hide }">
-        <div class="p-3 d-flex flex-column justify-content-between">
+        <div class="p-3 d-flex flex-column justify-content-between" style="display: block; height: 100%">
           <div>
-            <h2 id="sidebar-no-header-title">ArtWalk</h2>
+            <router-link to="/admin/main" id="sidebar-no-header-title" class="mb-4 tdn menulist" style="font-size: 2rem">ArtWalk</router-link>
             <hr>
             <nav class="my-3">
               <b-nav vertical>
+                <b-nav-item @click="hide" class="my-3">
+                  <router-link to="/admin/board/user" class="tdn menulist" style="font-size: 1.5rem">User Board</router-link>
+                </b-nav-item>
+                <b-nav-item @click="hide" class="my-3">
+                  <router-link to="/admin/board/route" class="tdn menulist" style="font-size: 1.5rem">Route Board</router-link>
+                </b-nav-item>
+                <b-nav-item @click="hide" class="my-3">
+                  <router-link to="/admin/board/record" class="tdn menulist" style="font-size: 1.5rem">Record Board</router-link>
+                </b-nav-item>
                 <b-nav-item active @click="hide" class="my-3">
-                  <router-link to="/admin/login" class="tdn menulist">Login</router-link>
-                </b-nav-item>
-                <b-nav-item @click="hide" class="my-3">
-                  <router-link to="/admin/board/user" class="tdn menulist">User Board</router-link>
-                </b-nav-item>
-                <b-nav-item @click="hide" class="my-3">
-                  <router-link to="/admin/board/record" class="tdn menulist">Record Board</router-link>
+                  <router-link to="/" class="tdn logout" style="font-size: 1.5rem">Logout</router-link>
                 </b-nav-item>
               </b-nav>
             </nav>
           </div>
           <div class="d-flex justify-content-end">
-            <b-button @click="hide" style="font-size: 1.5rem" variant="light">
-              <b-icon icon="chevron-double-left"></b-icon>
+            <b-button @click="hide" style="font-size: 2rem" variant="light">
+              <b-icon icon="chevron-double-left" class="pb-2"></b-icon>
             </b-button>
           </div>
         </div>
@@ -39,10 +42,9 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #07A794;
 }
 
 .tdn {
@@ -53,12 +55,24 @@
   color: #3c3c3c;
 }
 
+.logout {
+  color: #3c3c3c;
+}
+
 .menulist:hover {
   color: #07A794;
 }
 
+.logout:hover {
+  color: #D32F2F;
+}
+
 .maincolor {
   color: #07A794;
+}
+
+.maincolor:hover {
+  color: #FF9691;
 }
 
 </style>
