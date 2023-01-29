75d7de1c89d3226d9468a9e1e3a87b92af1f989b
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-01-29 20:15:29 +0900

    Fix: 수정 - viewType 못찾는 문제 해결 [S08P12A401-42]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/list/RouteListAdapter.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/list/RouteListAdapter.kt
@@ -1,16 +1,17 @@
 package com.a401.artwalk.view.route.list
 
 import android.view.ViewGroup
-import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.ListAdapter
 import com.a401.artwalk.BR
+import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseViewHolder
+import com.a401.artwalk.databinding.ItemRouteListBinding
 
-class RouteListAdapter : ListAdapter<RouteListItem, BaseViewHolder<ViewDataBinding>>(RouteListItem.diffUtil) {
+class RouteListAdapter : ListAdapter<RouteListItem, BaseViewHolder<ItemRouteListBinding>>(RouteListItem.diffUtil) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ViewDataBinding> = BaseViewHolder(parent, viewType)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ItemRouteListBinding> = BaseViewHolder(parent, R.layout.item_route_list)
 
-    override fun onBindViewHolder(holder: BaseViewHolder<ViewDataBinding>, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder<ItemRouteListBinding>, position: Int) {
         holder.binding.setVariable(BR.item, currentList[position])
         holder.binding.executePendingBindings()
     }
