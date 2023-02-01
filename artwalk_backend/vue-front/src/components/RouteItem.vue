89644cca2ec83a06869eb82f87a0336b9b163c2f
yujj25 <yujj25@naver.com>
2023-02-01 14:39:35 +0900

    Feat: 생성 - 회원별 경로 조회 기능 [S08P12A401-260]

--- artwalk_backend/vue-front/src/components/RouteItem.vue
+++ artwalk_backend/vue-front/src/components/RouteItem.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>
-  </div>
+  <b-col cols="3">
+    <b-card
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2 m-click"
+        @click="goDetail"
+    >
+      <b-card-title>
+        {{ route.title }}
+      </b-card-title>
+
+      <b-card-img>
+      </b-card-img>
+
+      <b-card-text>
+        <p>Route Id : {{ route.routeId }}</p>
+        <p>{{ route.userId }}</p>
+        <p>{{ formattedCreation }}</p>
+      </b-card-text>
+
+      <b-card-sub-title>
+        <p>Made by - {{ route.maker }}</p>
+      </b-card-sub-title>
+
+    </b-card>
+  </b-col>
 </template>
 
 <script>
+
+import dayjs from "dayjs";
 export default {
   name: "RouteItem.vue",
   props: {
     route: Object,
   },
+  data() {
+    return {
+      date: this.route.creation,
+      formattedCreation: dayjs(this.date).format('YYYY/MM/DD')
+    }
+  },
+  methods: {
+    goDetail() {
+      this.$router.push({ name: 'routeDetail', params: { routeId: this.route.routeId } })
+    }
+  }
 }
 </script>
 
