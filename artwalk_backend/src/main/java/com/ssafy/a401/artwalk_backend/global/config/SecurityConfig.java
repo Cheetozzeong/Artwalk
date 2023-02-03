317df481b3c38e61e361c2fd12ff6cfec9629fa7
공예찬 <rhddpcks9814@naver.com>
2023-02-03 17:19:21 +0900

    Fix: security config auth/login 수정

--- artwalk_backend/src/main/java/com/ssafy/a401/artwalk_backend/global/config/SecurityConfig.java
+++ artwalk_backend/src/main/java/com/ssafy/a401/artwalk_backend/global/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
 				.antMatchers( // 관리자 로그인, 사용자 인증 패이지는 토큰 없이 접근 가능
 					// "/**"
 					"/admin/**", "/css/**", "/js/**", "/favicon.ico",
-					// "/auth/login/*"
+					"/auth/login/*",
 					"/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**"
 					// TODO: 공유 페이지 토큰 예외 처리
 					);
