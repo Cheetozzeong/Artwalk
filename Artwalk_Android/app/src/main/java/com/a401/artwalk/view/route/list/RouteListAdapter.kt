76f4eed4bc5b786de4b45a87c446bfe8f2a2b30d
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-02-05 02:06:02 +0900

    Feat: 추가 - usecase 사용해서 경로 불러오기 [S08P12A401-255]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/list/RouteListAdapter.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/list/RouteListAdapter.kt
@@ -1,14 +1,31 @@
 package com.a401.artwalk.view.route.list
 
+import android.content.Context
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.a401.artwalk.BR
+import com.a401.artwalk.BuildConfig
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseViewHolder
 import com.a401.artwalk.databinding.ItemRouteListBinding
 import com.a401.domain.model.RouteForList
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 
-class RouteListAdapter(private val startButtonClickListener: StartButtonClickListener) : ListAdapter<RouteListItem, BaseViewHolder<ItemRouteListBinding>>(RouteListItem.diffUtil) {
+class RouteListAdapter(
+    private val startButtonClickListener: StartButtonClickListener,
+) : ListAdapter<RouteListItem, BaseViewHolder<ItemRouteListBinding>>(RouteListItem.diffUtil) {
+
+    private lateinit var context: Context
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        context = recyclerView.context
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ItemRouteListBinding> = BaseViewHolder(parent, R.layout.item_route_list)
 
@@ -16,6 +33,21 @@ class RouteListAdapter(private val startButtonClickListener: StartButtonClickLis
         holder.binding.setVariable(BR.item, currentList[position])
         holder.binding.executePendingBindings()
         holder.binding.startButtonClickListener = startButtonClickListener
+
+        holder.apply {
+
+            val accessToken = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDA3YmFlQG5hdmVyLmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE2NzU1NDkxOTZ9.t5oX3SgA6qVEzhpqiu0emvM7j_k7XcFtH5A-zSptvYdkZaIGCCTH_cAZCwxZdo0nVh4OaNZwEP1mytqWPhHU4A"
+
+            val routeId = currentList[position].routeForList.routeId
+            Glide.with(context)
+                .load(
+                    GlideUrl(
+                        BuildConfig.IMAGE_BASE_URL + routeId,
+                        LazyHeaders.Builder().addHeader("accessToken", accessToken).build()
+                    )
+                )
+                .into(binding.imageViewRouteItemThumbnail)
+        }
     }
 }
 
--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/list/RouteListViewModel.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/route/list/RouteListViewModel.kt
@@ -1,19 +1,19 @@
 package com.a401.artwalk.view.route.list
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.a401.artwalk.base.BaseViewModel
 import com.a401.artwalk.di.dispatcher.DispatcherProvider
 import com.a401.domain.model.RouteForList
+import com.a401.domain.usecase.FetchRouteList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
-import java.util.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.collections.ArrayList
 
 @HiltViewModel
 class RouteListViewModel @Inject constructor(
-    dispatcherProvider: DispatcherProvider
+    dispatcherProvider: DispatcherProvider,
+    private val fetchRouteList: FetchRouteList
 ) : BaseViewModel(dispatcherProvider) {
 
     private val routeList = MutableStateFlow<List<RouteForList>>(emptyList())
@@ -24,31 +24,15 @@ class RouteListViewModel @Inject constructor(
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     init {
-        val temp: ArrayList<RouteForList> = ArrayList()
-        temp.add(
-            RouteForList(
-                "",Date(),"aaa","eb{cFqbjfWMFyF{X|CiA",1
-            )
-        )
-        temp.add(
-            RouteForList(
-                "",Date(),"bbb","", 2
-            )
-        )
-        temp.add(
-            RouteForList(
-                "",Date(),"ccc","", 3
-            )
-        )
-        temp.add(
-            RouteForList(
-                "",Date(),"ddd","", 4
-            )
-        )
-
-        routeList.value = temp
+        getRoutes()
     }
 
-
-
+    private fun getRoutes() {
+        viewModelScope.launch {
+            // TODO: collect 어떻게 쓰는지 몰라서 collectLatest사용 !! 공부 필요 !!
+            fetchRouteList().collectLatest {
+                routeList.value = it
+            }
+        }
+    }
 }
