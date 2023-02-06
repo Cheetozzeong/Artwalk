b037cfea859a0853537397a917f4d4f97bde65d6
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-02-06 15:27:01 +0900

    Feat: 추가 - retrofit2 interceptor

--- Artwalk_Android/data/src/main/java/com/a401/data/api/ApiClient.kt
+++ Artwalk_Android/data/src/main/java/com/a401/data/api/ApiClient.kt
@@ -1,9 +1,12 @@
 package com.a401.data.api
 
 import com.a401.data.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+import java.io.IOException
 
 object ApiClient {
 
@@ -12,6 +15,23 @@ object ApiClient {
 
     private fun getInstance(baseUrl: String): Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
+        .client(provideOkHttpClient(AppInterceptor()))
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+
+    private fun provideOkHttpClient(interceptor: AppInterceptor): OkHttpClient = OkHttpClient.Builder().run {
+        addInterceptor(interceptor)
+        build()
+    }
+
+    class AppInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain) : Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("Accept", "application/json")
+                .build()
+            proceed(newRequest)
+        }
+    }
 }
