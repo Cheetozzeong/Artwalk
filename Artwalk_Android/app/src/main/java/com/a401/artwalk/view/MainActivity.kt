7a3623fcd451ccf626db1c57259c60eb1ce1a570
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-02-07 14:10:57 +0900

    Feat: 추가 - MainActivity-시작화면 [S08P12A401-408]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/MainActivity.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.a401.artwalk.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import com.a401.artwalk.view.login.LoginActivity
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseActivity
 import com.a401.artwalk.databinding.ActivityMainBinding
@@ -12,6 +15,18 @@ class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate)
         binding.imageViewMainLoading.animation = animation;
+        val pref = applicationContext.getSharedPreferences("a401Token", Context.MODE_PRIVATE)
+        val accessToken = pref.getString("accessToken", null)
+
+        val intent =
+            if(accessToken == null) {
+                Intent(this, LoginActivity::class.java)
+            }else {
+                Intent(this, SampleActivity::class.java)
+                // TODO: 자동 로그인
+                // TODO: 자동 로그인 실패시 login page로 이동
+            }
+        startActivity(intent)
     }
 
 }
