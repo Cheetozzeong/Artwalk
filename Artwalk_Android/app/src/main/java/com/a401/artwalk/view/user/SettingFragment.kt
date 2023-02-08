f1926b7e8062f0ce9a8bed1403fdfefb995d6d9f
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-02-08 16:19:30 +0900

    Layout: 추가 - 설정화면 [S08P12A401-414]

---
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/user/SettingFragment.kt
@@ -0,0 +1,8 @@
+package com.a401.artwalk.view.user
+
+import com.a401.artwalk.R
+import com.a401.artwalk.base.BaseFragment
+import com.a401.artwalk.databinding.FragmentSettingBinding
+
+class SettingFragment: BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+}
