bfc8612a125c034296351474beeb5e21ceff7b37
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-02-08 18:14:20 +0900

    policy ing

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/user/SettingFragment.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/user/SettingFragment.kt
@@ -1,8 +1,32 @@
 package com.a401.artwalk.view.user
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseFragment
 import com.a401.artwalk.databinding.FragmentSettingBinding
 
 class SettingFragment: BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textViewSettingPrivacyPolicy.setOnClickListener {
+            val action = SettingFragmentDirections.actionSettingToPolicy(
+                title = getString(R.string.privacy_policy_title),
+                policyContent = getString(R.string.privacy_policy_content)
+            )
+            findNavController().navigate(action)
+        }
+
+        binding.textViewSettingLocationPolicy.setOnClickListener {
+            val action = SettingFragmentDirections.actionSettingToPolicy(
+                title = getString(R.string.location_policy_title),
+                policyContent = getString(R.string.location_policy_content)
+            )
+            findNavController().navigate(action)
+        }
+    }
+
 }
