9bd1f6b50e1f7d27f7ba4f6be787fd886aa26e2c
xpeach1510 <xpeach1510@gmail.com>
2023-02-09 09:45:29 +0900

    Feat: 개인정보 및 서비스 이용약관 업데이트

--- artwalk_backend/src/main/java/com/ssafy/a401/artwalk_backend/global/config/SecurityConfig.java
+++ artwalk_backend/src/main/java/com/ssafy/a401/artwalk_backend/global/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
 					"/auth/login/*", "/auth/reg/*",
 					"/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**",
 					"/record/image/*",
-					"/sharing/*"
+					"/sharing/*", "/policy/privacy", "/policy/service"
 					);
 		};
 	}
