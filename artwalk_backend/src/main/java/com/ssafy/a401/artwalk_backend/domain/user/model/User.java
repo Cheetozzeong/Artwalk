5977cb841335eeefa8f181ef59066af3b0c2f709
Yechan Gong <rhddpcks9814@naver.com>
2023-02-09 12:39:20 +0900

    Fix: 회원가입 오류 해결(User setter 추가)

--- artwalk_backend/src/main/java/com/ssafy/a401/artwalk_backend/domain/user/model/User.java
+++ artwalk_backend/src/main/java/com/ssafy/a401/artwalk_backend/domain/user/model/User.java
@@ -17,6 +17,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,6 +28,7 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @DynamicInsert
