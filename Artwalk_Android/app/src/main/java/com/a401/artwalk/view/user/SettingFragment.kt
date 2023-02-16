f20487f22658cccbf53ff7737254a8602518fce8
ych526 <ych526@naver.com>
2023-02-16 12:32:10 +0900

    Feat: 추가 세팅 페이지 뒤로 가기 버튼 추가 및 LinearLayout으로 화면 전환되도록 변경[S08P12A401-445]

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/user/SettingFragment.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/user/SettingFragment.kt
@@ -2,6 +2,7 @@ package com.a401.artwalk.view.user
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.navigation.fragment.findNavController
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseFragment
@@ -12,7 +13,7 @@ class SettingFragment: BaseFragment<FragmentSettingBinding>(R.layout.fragment_se
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.textViewSettingPrivacyPolicy.setOnClickListener {
+        binding.linearLayoutSettingPrivacyPolicy.setOnClickListener {
             val action = SettingFragmentDirections.actionSettingToPolicy(
                 title = getString(R.string.privacy_policy_title),
                 policyContent = getString(R.string.privacy_policy_content)
@@ -20,12 +21,20 @@ class SettingFragment: BaseFragment<FragmentSettingBinding>(R.layout.fragment_se
             findNavController().navigate(action)
         }
 
-        binding.textViewSettingLocationPolicy.setOnClickListener {
+        binding.linearLayoutSettingLocationPolicy.setOnClickListener {
             val action = SettingFragmentDirections.actionSettingToPolicy(
                 title = getString(R.string.location_policy_title),
                 policyContent = getString(R.string.location_policy_content)
             )
             findNavController().navigate(action)
+        }
+        setBackButton()
+    }
+    
+    private fun setBackButton(){
+        val backButton = view?.findViewById<ImageButton>(R.id.ImageButton_setting_back)
+        backButton?.setOnClickListener{
+            findNavController().popBackStack()
         }
     }
 
