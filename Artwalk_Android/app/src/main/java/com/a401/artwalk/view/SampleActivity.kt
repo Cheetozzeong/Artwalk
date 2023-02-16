3342a64bbeb2cec9f643123495932749fc630ec4
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-02-16 13:59:39 +0900

    Feat: 추가 - activity 시작시 서비스 생성

--- Artwalk_Android/app/src/main/java/com/a401/artwalk/view/SampleActivity.kt
+++ Artwalk_Android/app/src/main/java/com/a401/artwalk/view/SampleActivity.kt
@@ -1,21 +1,27 @@
 package com.a401.artwalk.view
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.a401.artwalk.R
 import com.a401.artwalk.base.BaseActivity
 import com.a401.artwalk.databinding.SampleViewBinding
+import com.a401.artwalk.view.record.RecordService
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SampleActivity: BaseActivity<SampleViewBinding>(R.layout.sample_view) {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setNavigation()
+        this.applicationContext.getSystemService(RecordService::class.java) ?: startService(Intent(this.applicationContext, RecordService::class.java))
     }
 
     private fun setNavigation() {
